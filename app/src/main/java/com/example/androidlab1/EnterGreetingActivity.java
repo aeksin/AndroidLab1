75606e14434c850b4adb549b521ed2d06adb9ae0
aeksin <113206813+aeksin@users.noreply.github.com>
2024-10-29 21:48:58 +0300

    first commit

---
+++ app/src/main/java/com/example/androidlab1/EnterGreetingActivity.java
@@ -0,0 +1,45 @@
+package com.example.androidlab1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EnterGreetingActivity extends Activity {
+    private static final int START_ENTER_NAME_ACTIVITY=1;
+    private TextView greetingTextView;
+    private EditText greetingEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_activity);
+        greetingTextView = findViewById( R.id.greetingTextView);
+        greetingEditText = findViewById( R.id.greetingEditText);
+
+    }
+
+    public void goToNextWindow(View v){
+        Intent intent = new Intent(EnterGreetingActivity.this, EnterNameActivity.class);
+        intent.putExtra("GREETING",greetingEditText.getText().toString());
+        startActivityForResult(intent, 1);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case (START_ENTER_NAME_ACTIVITY):
+                if (resultCode == 1 && data!=null){
+                    String greeting = data.getStringExtra("GREETING");
+                    String credentials = data.getStringExtra("NAME");
+                    greetingTextView.setText(greeting+" "+ credentials + "!");
+                }
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/androidlab1/EnterNameActivity.java
@@ -0,0 +1,35 @@
+package com.example.androidlab1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EnterNameActivity extends Activity {
+    private EditText enterNameEditText;
+    private Button enterNameButton;
+    private String greeting;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+        enterNameEditText = findViewById( R.id.enterNameEditText);
+        enterNameButton = findViewById( R.id.enterNameButton);
+        Intent callerIntent = getIntent();
+        greeting = callerIntent.getStringExtra("GREETING");
+
+        enterNameButton.setOnClickListener(v -> {
+            Intent intent = new Intent(EnterNameActivity.this, EnterGreetingActivity.class);
+            intent.putExtra("GREETING", greeting);
+            intent.putExtra("NAME", enterNameEditText.getText().toString());
+            setResult(1, intent);
+            finish();
+        });
+
+    }
+}
